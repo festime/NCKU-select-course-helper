a5096c482b9cdf9960502d53915ce50a435a0d51
Yulin Chen <gloverock2@gmail.com>
2015-05-11 09:19:36 +0800

    Users can see general education courses in their free time.

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -6,8 +6,20 @@ class CoursesController < ApplicationController
   end
 
   def search
-    binding.pry
-    redirect_to front_path
+    @courses = {}
+    params[:core].each do |type, selected|
+      type = type.to_sym
+      result = available_general_education_classes(:core, type)
+      @courses[type] = result
+    end
+    params[:cross].each do |type, selected|
+      type = type.to_sym
+      result = available_general_education_classes(:cross, type)
+      @courses[type] = result
+    end
+
+    render :front
+    #redirect_to front_path
   end
 
   private
@@ -44,28 +56,28 @@ class CoursesController < ApplicationController
     result
   end
 
-  def available_general_education_classes(category, sub_category)
+  def available_general_education_classes(type, sub_type)
     data_hash = {
       core: {
-        philosophy_art: {category: "哲學與藝術", model: GeneralEducation},
-        citizenship_history: {category: "公民與歷史", model: CitizenshipHistory},
-        basic_chinese: {category: "基礎國文", model: BasicChinese},
-        freshman_english: {category: "大一英文", model: InternationalLanguage},
-        sophomore_english: {category: "大二英文", model: InternationalLanguage}
+        philosophy_art: {type: "哲學與藝術", model: GeneralEducation},
+        citizenship_history: {type: "公民與歷史", model: CitizenshipHistory},
+        basic_chinese: {type: "基礎國文", model: BasicChinese},
+        freshman_english: {type: "大一英文", model: InternationalLanguage},
+        sophomore_english: {type: "大二英文", model: InternationalLanguage}
       },
       cross: {
-        humanities: {category: "人文學", model: GeneralEducation},
-        life_science_health: {category: "生命科學與健康", model: GeneralEducation},
-        science_engineering: {category: "自然與工程科學", model: GeneralEducation},
-        social_science: {category: "社會科學", model: GeneralEducation},
-        integrated: {category: "科際整合", model: GeneralEducation}
+        humanities: {type: "人文學", model: GeneralEducation},
+        life_science_health: {type: "生命科學與健康", model: GeneralEducation},
+        science_engineering: {type: "自然與工程科學", model: GeneralEducation},
+        social_science: {type: "社會科學", model: GeneralEducation},
+        integrated: {type: "科際整合", model: GeneralEducation}
       }
     }
 
-    search_term = data_hash[category][sub_category][:category]
-    model = data_hash[category][sub_category][:model]
+    search_term = data_hash[type][sub_type][:type]
+    model = data_hash[type][sub_type][:model]
 
-    if params[category][sub_category].present?
+    if params[type][sub_type].present?
       collection_of_id = model.where(category: search_term).pluck(:id)
       schedules = model.where(category: search_term).pluck(:schedule).collect do |schedule|
         handle_schedule!(schedule)
@@ -86,33 +98,9 @@ class CoursesController < ApplicationController
         end
       end
 
-      model.find(id_of_satisfied_courses)
+      return model.find(id_of_satisfied_courses)
     end
-  end
 
-  def available_chinese_courses
-    if params[:core][:basic_chinese].present?
-      collection_of_id = BasicChinese.where(category: "基礎國文").pluck(:id)
-      schedules = BasicChinese.where(category: "基礎國文").pluck(:schedule).collect do |schedule|
-        handle_schedule!(schedule)
-      end
-
-      id_of_satisfied_courses = []
-      schedules.each_with_index do |schedule, index|
-        schedule.each do |day, array_of_time|
-          check = true
-          array_of_time.each do |time|
-            if !params[:freetime][day].include? time
-              check = false
-              break
-            end
-          end
-
-          id_of_satisfied_courses << collection_of_id[index] if check
-        end
-      end
-
-      BasicChinese.find(id_of_satisfied_courses)
-    end
+    return []
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def type_english_to_chinese(type)
+    hash = {
+      philosophy_art: "哲學與藝術",
+      citizenship_history: "公民與歷史",
+      basic_chinese: "基礎國文",
+      freshman_english: "大一英文",
+      sophomore_english: "大二英文",
+      humanities: "人文學",
+      life_science_health: "生命科學與健康",
+      science_engineering: "自然與工程科學",
+      social_science: "社會科學",
+      integrated: "科際整合"
+    }
+    hash[type]
+  end
 end
